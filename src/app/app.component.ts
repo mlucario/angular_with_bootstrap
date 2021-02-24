0229424748a96eb0c1efdd39991e88c61a4b48a1
Quy Nguyen <nhonquy@gmail.com>
2021-02-23 16:19:42 -0800

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isLoading: boolean = false;
   title = 'angular-bootstrap-review-with-expressjs';
 }
